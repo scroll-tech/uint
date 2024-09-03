eba5ed93b34db54d7deb9cda2cbc392102af8e08
lightsing <light.tsing@gmail.com>
2024-09-03 16:37:59 +0800

    add check_bytes

--- src/support/rkyv.rs
+++ src/support/rkyv.rs
@@ -3,8 +3,9 @@
 #![cfg(feature = "rkyv")]
 #![cfg_attr(docsrs, doc(cfg(feature = "rkyv")))]
 
+use std::convert::Infallible;
 use crate::Uint;
-use rkyv::{Archive, Archived, Deserialize, Fallible, Serialize};
+use rkyv::{Archive, Archived, CheckBytes, Deserialize, Fallible, Serialize};
 
 impl<const BITS: usize, const LIMBS: usize> Archive for Uint<BITS, LIMBS> {
     type Archived = Uint<BITS, LIMBS>;
@@ -13,6 +14,14 @@ impl<const BITS: usize, const LIMBS: usize> Archive for Uint<BITS, LIMBS> {
     #[inline]
     unsafe fn resolve(&self, pos: usize, resolver: Self::Resolver, out: *mut Self::Archived) {
         self.limbs.resolve(pos, resolver, out as *mut [u64; LIMBS]);
+    }
+}
+
+impl<C: ?Sized, const BITS: usize, const LIMBS: usize> CheckBytes<C> for Uint<BITS, LIMBS>
+{
+    type Error = Infallible;
+    unsafe fn check_bytes<'a>(value: *const Self, _: &mut C) -> Result<&'a Self, Self::Error> {
+        Ok(&*value)
     }
 }
 
